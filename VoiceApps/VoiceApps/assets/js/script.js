99813e4d5c8b58c55997beaf26f68376d1bbf89b
denisSorokinz <51695697+denisSorokinz@users.noreply.github.com>
2019-07-04 00:22:02 +0300

    Add files via upload

---
+++ VoiceApps/VoiceApps/assets/js/script.js
@@ -0,0 +1,33 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+
+$(".to-home").click(function() {
+    $('html,body').animate({
+        scrollTop: 0},
+        'slow');
+})
+
+$(".to-stuff").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".promo-1").offset().top - 58},
+        'slow');
+})
+
+$(".to-apps").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".promo-2").offset().top - 58},
+        'slow');
+})
+
+$(".to-build").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".promo-3").offset().top - 58},
+        'slow');
+})
