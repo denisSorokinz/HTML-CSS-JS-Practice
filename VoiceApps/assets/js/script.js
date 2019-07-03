759b4331edcff7d92d2bcfd00f4366d033d967a0
denisSorokinz <d.sorokin@gmail.com>
2019-07-04 00:25:55 +0300

    'changed'

--- VoiceApps/assets/js/script.js
+++ VoiceApps/assets/js/script.js
@@ -1,3 +1,13 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+
 $(".to-home").click(function() {
     $('html,body').animate({
         scrollTop: 0},
@@ -6,32 +16,14 @@ $(".to-home").click(function() {
 
 $(".to-stuff").click(function() {
     $('html,body').animate({
-        scrollTop: $(".promo-1").offset().top + 100},
-        'slow');
-})
-
-$(".to-stuff").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".promo-1").offset().top - 100},
-        'slow');
-})
-
-$(".to-stuff").click(function() {
-    $('html,body').animate({
         scrollTop: $(".promo-1").offset().top - 58},
-        'fast');
-})
-
-$(".to-apps").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".promo-2").offset().top + 100},
         'slow');
 })
 
 $(".to-apps").click(function() {
     $('html,body').animate({
         scrollTop: $(".promo-2").offset().top - 58},
-        'fast');
+        'slow');
 })
 
 $(".to-build").click(function() {
