9835863218cc74a1c8cc222221dda4abff4265f7
denisSorokinz <d.sorokin@gmail.com>
2019-07-04 00:51:22 +0300

    'changed'

--- VoiceApps/assets/js/script.js
+++ VoiceApps/assets/js/script.js
@@ -1,6 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block"
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
@@ -12,22 +13,26 @@ $(".to-home").click(function() {
     $('html,body').animate({
         scrollTop: 0},
         'slow');
+    document.getElementById("mySidebar").style.width = "0";
 })
 
 $(".to-stuff").click(function() {
     $('html,body').animate({
         scrollTop: $(".promo-1").offset().top - 58},
         'slow');
+    document.getElementById("mySidebar").style.width = "0";
 })
 
 $(".to-apps").click(function() {
     $('html,body').animate({
         scrollTop: $(".promo-2").offset().top - 58},
         'slow');
+    document.getElementById("mySidebar").style.width = "0";
 })
 
 $(".to-build").click(function() {
     $('html,body').animate({
         scrollTop: $(".promo-3").offset().top - 58},
         'slow');
+    document.getElementById("mySidebar").style.width = "0";
 })
