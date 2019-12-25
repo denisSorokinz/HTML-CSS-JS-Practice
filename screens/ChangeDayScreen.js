f8ff9e63f13dc2c1ca5e1d26feaea9de280e3f00
denisSorokinz <d.sorokin@gmail.com>
2019-12-25 11:47:46 +0200

    'upd'

--- screens/ChangeDayScreen.js
+++ screens/ChangeDayScreen.js
@@ -3,28 +3,21 @@ import { SafeAreaView, Text } from 'react-native';
 import styles from './styles';
 
 export default class ChangeDayScreen extends Component {
-    static navigationOptions = {
-        title: 'this.state.dayName',
-        headerVisible: true
+
+    static navigationOptions = (props) => {
+        return {
+            title: props.navigation.state.params.dayName,
+            headerVisible: true
+        }
     };
 
     constructor(props) {
         super();
-        this.state = {
-            dayName: 'DAY OF WEEK'
-        }
-    }
-
-    componentDidMount() {
-        this.setState({
-            dayName: this.props.navigation.state.params.dayName
-        });
-        console.log(this.state.dayName);
     }
 
     render() {
         return (
-            <SafeAreaView style={styles.container}>
+            <SafeAreaView>
               <Text>
                   TEST
               </Text>
