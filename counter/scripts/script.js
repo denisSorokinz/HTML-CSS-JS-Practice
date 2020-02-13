12390cb97d684697b27328b5b9f2dc9379c978d0
denisSorokinz <d.sorokin@gmail.com>
2020-02-13 22:51:38 +0200

    '+counter'

---
+++ counter/scripts/script.js
@@ -0,0 +1,23 @@
+var timeLeftDiv = document.querySelector('#timeLeft');
+var millisecondsInDay = 86400000;
+var millisecondsInHour = 3600000;
+var millisecondsInMinute = 60000;
+var millisecondsInSecond = 1000;
+var total = 0;
+function init() {
+    setInterval(function () {
+        console.log('YAY');
+        total = new Date(2021, 7, 26).getTime() - Date.now();
+        timeLeftDiv.innerHTML = dhm(total);
+    }, 1000);
+}
+function dhm(ms) {
+    var days = Math.floor(ms / millisecondsInDay);
+    var daysms = ms % millisecondsInDay;
+    var hours = Math.floor((daysms) / millisecondsInHour);
+    var hoursms = ms % millisecondsInHour;
+    var minutes = Math.floor((hoursms) / millisecondsInMinute);
+    var minutesms = ms % millisecondsInMinute;
+    var seconds = Math.floor((minutesms) / millisecondsInSecond);
+    return "DAYS: " + days + "</br>\n            HOURS: " + hours + "</br>\n            MINUTES: " + minutes + "</br>\n            SECONDS: " + seconds;
+}
