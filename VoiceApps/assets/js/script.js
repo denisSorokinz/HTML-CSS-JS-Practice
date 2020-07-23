026225a6afecaf6ebd4fadd13cbd35424e309128
denis_sorokin_ua <d.sorokin@gmail.com>
2020-07-23 19:09:16 +0300

    sidebar animation fix

--- VoiceApps/assets/js/script.js
+++ VoiceApps/assets/js/script.js
@@ -1,7 +1,6 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "100%";
-    document.getElementById("mySidebar").style.display = "block"
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
